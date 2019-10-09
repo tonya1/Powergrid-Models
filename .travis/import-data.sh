440b7b84f107f4f3a9cb314ece29fda064171057
Tonya Martin <tonya.martin@pnnl.gov>
2019-10-09 11:47:14 -0700

    travis timinig issues

--- .travis/import-data.sh
+++ .travis/import-data.sh
@@ -196,7 +196,7 @@ wc -l *txt | grep total | awk '{print $1}'
 
 echo " "
 echo "Loading measurements files"
-ls -1 *txt | xargs -P 1 -n 1 -I, bash -c 'echo "  $(date) Loading measurments file ,";python3 ../InsertMeasurements.py ,'
+ls -1 *txt | xargs -P 2 -n 1 -I, bash -c 'echo "  $(date) Loading measurments file ,";python3 ../InsertMeasurements.py ,'
 
 echo " "
 rangeCount=`curl -s -G -H 'Accept: application/xml' "${url_blazegraph}sparql" --data-urlencode ESTCARD | sed 's/.*rangeCount=\"\([0-9]*\)\".*/\1/'`
